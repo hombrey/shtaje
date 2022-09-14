fec138469fcad7d0dc2951da5878159f13db1e40
Hombrey Escalante <hombrey.escalante@gmail.com>
2022-09-14 17:12:34 +0700

    introduce kindyPortal

---
+++ srcHTML/kindyPortal/functions.js
@@ -0,0 +1,135 @@
+//{{{variable declarations
+let urlNow;
+let assetDir,srcDir;
+let mamSelect,choiSelect,laSelect;
+let fgX;
+//}}}variable declarations
+
+//make sure elements are loaded before proceeding
+const checkElement = async selector => {
+  while ( document.querySelector(selector) === null) {
+    await new Promise( resolve =>  requestAnimationFrame(resolve) )
+  } //while ( document.querySelector(selector) === null)
+  return document.querySelector(selector); 
+}; //const checkElement = async selector
+
+window.onload = initWin();
+async function initWin() {
+
+    await delay (40);
+
+    srcDir = document.getElementById("srcdir").innerHTML; 
+    //Get location of lesson assets
+    assetDir = document.getElementById("assetdir").innerHTML;
+
+    mamSelect = document.getElementById("selectMam");
+    choiSelect = document.getElementById("selectChoi");
+    laSelect = document.getElementById("selectLa");
+    fgX = document.getElementById("foregroundX");
+
+    mamSelect.focus();
+    fgX.src=srcDir+"img/mam.webp";
+} //function initWin()
+
+window.addEventListener("keydown", evalKeyDown2, false); //capture keypress on bubbling (false) phase
+function evalKeyDown2(evnt) {
+    let keyPressed = evnt.keyCode;
+    console.log ("Pressed2:", keyPressed);
+    switch (keyPressed) {
+    case 13 :  evnt.preventDefault();
+                openNewTab(urlNow);
+                break; //return
+    case 77 : if(!event.shiftKey) mamSelect.focus(); 
+                fgX.src=srcDir+"img/mam.webp";
+               break; //key: m
+    case 67 : if(!event.shiftKey) choiSelect.focus(); 
+                fgX.src=srcDir+"img/choi.webp";
+               break; //key: c
+    case 76 : if(!event.shiftKey) laSelect.focus(); 
+               fgX.src=srcDir+"img/la.webp";
+             break; //key: l
+    case 87  : if(!event.shiftKey) parent.postMessage("FocusSeq","*");
+                else parent.postMessage("FocusTool","*"); 
+                break; //key: w
+        default : return;
+    } //switch (keyPressed)
+} //evalKey(event)
+
+window.addEventListener('message', evalMessage);
+function evalMessage (evnt) {
+    // Get the sent data
+    var data = evnt.data;
+    //console.log ("message received");
+
+    if (data == "FocusIframe") {
+        //console.log("focusDummy");
+        document.getElementById('selectbox').focus();
+    }
+} //function evalMessage(event)
+
+function openNewTab(urlvalue) {
+    //window.open(urlvalue,'_blank');
+    window.open(urlvalue,'_self');
+} //function openNewTab(urlvalue)
+
+
+function selectFile(urlvalue) {
+    urlNow = urlvalue;
+} //function openNewTab(urlvalue)
+
+
+//{{{helper functions
+function createHelpWindow() {
+    helpHandle = document.createElement('iframe');
+    helpHandle.setAttribute('id','myHelpFrame');
+    helpHandle.setAttribute('class','hiddenHelp');
+    helpHandle.setAttribute('src',srcDir+'help.html');
+    document.body.appendChild(helpHandle);
+} //function createHelpWindow()
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.start = function(){
+        this.sound.pause();
+        this.sound.currentTime = 0;
+        this.sound.play();
+    } //this.start = function(){
+    this.pause = function(){
+        this.sound.pause();
+    } //this.play = function(){
+    this.play = function(){
+        this.sound.play();
+    } //this.play = function(){
+    this.stop = function(){
+        this.sound.pause();
+        this.sound.currentTime = 0;
+        isPaused = true;
+    }//this.stop = function()    
+    this.sound.onended = function () {
+        isPaused = true;
+        pauseIndicator.style.display = "none";
+    }; //sound.onended
+}//function sound(src)
+
+function insertCss( code ) {
+    var style = document.createElement('style');
+    style.innerHTML = code;
+
+    document.getElementsByTagName("head")[0].appendChild( style );
+} //function insertCss( code)
+
+function delay(n) {  
+        n = n || 2000;
+        return new Promise(done => {
+                setTimeout(() => {
+                        done();
+                        }, n);
+            });
+}//function delay()
+
+//}}}helper functions
---
+++ srcHTML/kindyPortal/genKP.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# extract the source directory from the command used to call this script
+SOURCEDIR=`echo "${0%/*}"`
+ASSETDIR='./'
+#truncate last character (i.e. "/")
+
+input="/tmp/list.txt"
+
+echo "<!DOCTYPE html>" > index.html
+echo "<html lang=\"en\">" >> index.html
+echo "<head>" >> index.html
+echo "    <meta charset=\"UTF-8\">" >> index.html
+echo "    <title>Kindy</title> " >> index.html
+echo "    <meta name=\"description\" content=\"select lessons from here\">" >> index.html
+echo "    <meta name=\"author\" content=\"Hombrey\">" >> index.html
+echo "    <link rel=\"stylesheet\" href=\"$SOURCEDIR/styles.css\">" >> index.html
+echo "    <script src=\"$SOURCEDIR/functions.js\"></script> " >> index.html
+echo "</head>" >> index.html
+echo "" >> index.html
+echo "<body id=\"myBody\">" >> index.html
+echo "    <div id=\"srcdir\" style=\"display:none;\">$SOURCEDIR/</div>" >> index.html
+echo "    <div id=\"assetdir\" style=\"display:none\">./</div>" >> index.html
+echo "" >> index.html
+echo "    <img class=\"fullPage\" id=\"backgroundX\" src=\"$SOURCEDIR/img/BG0.webp\">" >> index.html
+echo "    <img class=\"fullPage\" id=\"foregroundX\" src=\"$SOURCEDIR/img/null.webp\">" >> index.html
+
+echo "     <div class=\"gridbox\">" >> index.html
+echo "" >> index.html
+echo "          <select id=\"selectMam\" name=\"\" onChange=\"selectFile(this.value)\" onclick=\"openNewTab(this.value)\" size=15>" >> index.html
+
+ls -d ./LI1*/*/*.html > /tmp/listUnsort.txt
+sort --version-sort /tmp/listUnsort.txt> $input
+
+    while IFS= read -r line
+    do
+        truncLine=${line#*/*/*MH}
+        echo "          <option value=\"$line\">$truncLine</option>" >> index.html
+    done < "$input"
+
+echo "          </select>" >> index.html
+
+echo "" >> index.html
+echo "          <select id=\"selectChoi\" name=\"\" onChange=\"selectFile(this.value)\" onclick=\"openNewTab(this.value)\" size=15>" >> index.html
+
+ls -d ./LI2*/*/*.html > /tmp/listUnsort.txt
+sort --version-sort /tmp/listUnsort.txt> $input
+
+    while IFS= read -r line
+    do
+        truncLine=${line#*/*/*CH}
+        echo "          <option value=\"$line\">$truncLine</option>" >> index.html
+    done < "$input"
+
+echo "          </select>" >> index.html
+
+echo "" >> index.html
+echo "          <select id=\"selectLa\" name=\"\" onChange=\"selectFile(this.value)\" onclick=\"openNewTab(this.value)\" size=15>" >> index.html
+
+ls -d ./LI3*/*/*.html > /tmp/listUnsort.txt
+sort --version-sort /tmp/listUnsort.txt> $input
+
+    while IFS= read -r line
+    do
+        truncLine=${line#*/*/*LH}
+        echo "          <option value=\"$line\">$truncLine</option>" >> index.html
+    done < "$input"
+
+echo "          </select>" >> index.html
+
+
+echo "     </div>  <!-- gridbox --> " >> index.html
+echo "</body> " >> index.html
+
+echo "</html>" >> index.html

20bb486d9ebef7f16b968d671e98fd6a76c6ce8a
Hombrey Escalante <hombrey.escalante@gmail.com>
2023-03-12 11:34:14 +0700

    FrameSeq - make main generator more flexible. Change bg to make it easier to prepare images with transparent BGs

--- srcHTML/FrameSeq/genFramerEB.sh
+++ srcHTML/FrameSeq/genFramerEB.sh
@@ -30,7 +30,20 @@ echo "    <!--}}}head-->" >> 0_FrameX.html
 echo "<body>" >> 0_FrameX.html
 echo "    <!--{{{outerFrame-->" >> 0_FrameX.html
 echo " <img class=\"fullPage\" id=\"backgroundX\" src=\"$SOURCEDIR/background.jpg\">" >> 0_FrameX.html
-echo " <iframe class=\"subframe\" id=\"myIframe\" src=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\"></iframe>" >> 0_FrameX.html
+case $LESSONDIR in
+    *"LI1_Mam"*)
+		echo " <iframe class=\"subframe\" id=\"myIframe\" src=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\"></iframe>" >> 0_FrameX.html
+    ;;
+    *"LI2_Choi"*)
+		echo " <iframe class=\"subframe\" id=\"myIframe\" src=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\"></iframe>" >> 0_FrameX.html
+    ;;
+    *"LI3_La"*)
+		echo " <iframe class=\"subframe\" id=\"myIframe\" src=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\"></iframe>" >> 0_FrameX.html
+    ;;
+	*)
+		echo " <iframe class=\"subframe\" id=\"myIframe\" src=\"$SOURCEROOT/startpage/index.html\"></iframe>" >> 0_FrameX.html
+	;;
+esac
 echo "" >> 0_FrameX.html
 echo "    <select id=\"toolSelect\" onClick=\"setToolSource(this.value)\" size=1 autofocus> " >> 0_FrameX.html
 
@@ -40,18 +53,23 @@ echo "   </select>" >> 0_FrameX.html
 echo "    <!--}}}outerFrame-->" >> 0_FrameX.html
 
 echo "    <select id=\"seqSelect\" onChange=\"setSeqSource()\" size=1> " >> 0_FrameX.html
-echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\">0_hello</option> " >> 0_FrameX.html
 case $LESSONDIR in
     *"LI1_Mam"*)
+		echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\">0_hello</option> " >> 0_FrameX.html
         echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/L1/x_horiz.html\">3_WarmUp</option> " >> 0_FrameX.html
     ;;
     *"LI2_Choi"*)
+		echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\">0_hello</option> " >> 0_FrameX.html
         echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/L2/x_horiz.html\">3_WarmUp</option> " >> 0_FrameX.html
     ;;
     *"LI3_La"*)
+		echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\">0_hello</option> " >> 0_FrameX.html
         echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/L3/x_horiz.html\">3_WarmUp</option> " >> 0_FrameX.html
+	;;
+	*)
+		echo "        <option value=\"$SOURCEROOT/startpage/index.html\">0_startPage</option> " >> 0_FrameX.html
+	;;
 esac
-echo "        <option value=\"$SOURCEROOT/Klyne/klyne.html\">9_classMgt</option> " >> 0_FrameX.html
 echo "" >> 0_FrameX.html
 
 #list html files in lesson plan root directory

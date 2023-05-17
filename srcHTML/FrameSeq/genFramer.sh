eeb8d1dadd3fe935d5c1720c1577cbda0a3bac14
Hombrey Escalante <hombrey.escalante@gmail.com>
2023-05-17 18:32:44 +0700

    GenFramer - integrate AP and EB generators

--- srcHTML/FrameSeq/genFramer.sh
+++ srcHTML/FrameSeq/genFramer.sh
@@ -29,8 +29,23 @@ echo "</head>" >> 0_FrameX.html
 echo "    <!--}}}head-->" >> 0_FrameX.html
 echo "<body>" >> 0_FrameX.html
 echo "    <!--{{{outerFrame-->" >> 0_FrameX.html
-echo " <img class=\"fullPage\" id=\"backgroundX\" src=\"$SOURCEDIR/background.jpg\">" >> 0_FrameX.html
-echo " <iframe class=\"subframe\" id=\"myIframe\" src=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\"></iframe>" >> 0_FrameX.html
+case $LESSONDIR in
+	*/KH*|*/PH*|*/SH*)
+		echo " <img class=\"fullPage\" id=\"backgroundX\" src=\"$SOURCEDIR/backgroundAP.jpg\">" >> 0_FrameX.html
+	;;
+	*)
+		echo " <img class=\"fullPage\" id=\"backgroundX\" src=\"$SOURCEDIR/background.jpg\">" >> 0_FrameX.html
+	;;
+esac
+
+case $LESSONDIR in
+    *"LI1_Mam"*| *"LI2_Choi"*| *"LI3_La"*)
+		echo " <iframe class=\"subframe\" id=\"myIframe\" src=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\"></iframe>" >> 0_FrameX.html
+    ;;
+	*)
+		echo " <iframe class=\"subframe\" id=\"myIframe\" src=\"$SOURCEROOT/startpage/index.html\"></iframe>" >> 0_FrameX.html
+	;;
+esac
 echo "" >> 0_FrameX.html
 echo "    <select id=\"toolSelect\" onClick=\"setToolSource(this.value)\" size=1 autofocus> " >> 0_FrameX.html
 
@@ -40,22 +55,31 @@ echo "   </select>" >> 0_FrameX.html
 echo "    <!--}}}outerFrame-->" >> 0_FrameX.html
 
 echo "    <select id=\"seqSelect\" onChange=\"setSeqSource()\" size=1> " >> 0_FrameX.html
-echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\">0_hello</option> " >> 0_FrameX.html
+
 case $LESSONDIR in
     *"LI1_Mam"*)
+		echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\">0_hello</option> " >> 0_FrameX.html
         echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/L1/x_horiz.html\">3_WarmUp</option> " >> 0_FrameX.html
+		echo "        <option value=\"$SOURCEROOT/Klyne/klyne.html\">9_classMgt</option> " >> 0_FrameX.html
     ;;
     *"LI2_Choi"*)
+		echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\">0_hello</option> " >> 0_FrameX.html
         echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/L2/x_horiz.html\">3_WarmUp</option> " >> 0_FrameX.html
+		echo "        <option value=\"$SOURCEROOT/Klyne/klyne.html\">9_classMgt</option> " >> 0_FrameX.html
     ;;
     *"LI3_La"*)
+		echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/x_horiz.html\">0_hello</option> " >> 0_FrameX.html
         echo "        <option value=\"$SOURCEPREROOT/SongsHTML/A_hi/L3/x_horiz.html\">3_WarmUp</option> " >> 0_FrameX.html
+		echo "        <option value=\"$SOURCEROOT/Klyne/klyne.html\">9_classMgt</option> " >> 0_FrameX.html
+	;;
+	*)
+		echo "        <option value=\"$SOURCEROOT/startpage/index.html\">0_startPage</option> " >> 0_FrameX.html
+	;;
 esac
-echo "        <option value=\"$SOURCEROOT/Klyne/klyne.html\">9_classMgt</option> " >> 0_FrameX.html
 echo "" >> 0_FrameX.html
 
 #list html files in lesson plan root directory
-ls -d *.html> $HOMEDIR/tmp/list.txt
+ls -d *.html > $HOMEDIR/tmp/list.txt
 input="$HOMEDIR/tmp/list.txt"
 arrayIndex=1;
 
@@ -69,7 +93,11 @@ do
 done < "$input"
 
 #list html files tucked in a subdirectory
-ls -d ./*/*.html> $HOMEDIR/tmp/list.txt
+ls -d ./*/*.html> $HOMEDIR/tmp/listUnsort.txt
+
+#this makes sure that the time tags are in order
+sort --version-sort $HOMEDIR/tmp/listUnsort.txt> $HOMEDIR/tmp/list.txt
+
 input="$HOMEDIR/tmp/list.txt"
 arrayIndex=1;
 

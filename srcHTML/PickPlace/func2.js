40a408dce2c6eed51bd9cde3ce4072462cef758e
Hombrey Escalante <hombrey.escalante@gmail.com>
2024-01-27 22:06:42 +0700

    pickandPlace - fix delay before init

--- srcHTML/PickPlace/func2.js
+++ srcHTML/PickPlace/func2.js
@@ -17,7 +17,7 @@ let helpHandle;
 // window.onload = initWin();
 window.addEventListener("resize", scaleScreen);
 // window.addEventListener("DOMContentLoaded", initWin);
-window.addEventListener("DOMContentLoaded", setTimeout(initWin,900));
+window.addEventListener("DOMContentLoaded", () => { setTimeout(initWin,200);} );
 
 //make sure elements are loaded before proceeding
 const checkElement = async selector => {
